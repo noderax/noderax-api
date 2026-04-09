fde18e15cecc931be784222530bcf981a18edbc7
Göktuğ Ceyhan <goktugceyhan@gmail.com>
2026-04-10 01:35:51 +0300

    Wire setup runtime filters and metrics

--- src/setup-app.module.ts
+++ src/setup-app.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { CorrelationIdMiddleware } from './common/middleware/correlation-id.middleware';
 import configuration from './config/configuration';
 import { normalizeDatabaseEnvAliases } from './config/database-env.utils';
 import { LegacyHealthController } from './legacy-health.controller';
 import { SetupModule } from './modules/setup/setup.module';
+import { RuntimeModule } from './runtime/runtime.module';
 
 normalizeDatabaseEnvAliases();
 
@@ -23,15 +27,22 @@ normalizeDatabaseEnvAliases();
         limit: 100,
       },
     ]),
+    RuntimeModule,
     SetupModule,
   ],
   controllers: [AppController, LegacyHealthController],
   providers: [
     AppService,
+    AllExceptionsFilter,
+    LoggingInterceptor,
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
     },
   ],
 })
-export class SetupAppModule {}
+export class SetupAppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorrelationIdMiddleware).forRoutes('*');
+  }
+}
