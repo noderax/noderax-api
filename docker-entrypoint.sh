03a681171ca7beeda7ecd1888e818ae2a700e3be
Göktuğ Ceyhan <goktugceyhan@gmail.com>
2026-04-15 16:40:00 +0300

    Remove startup chown overhead from API entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,6 @@
 set -eu
 
 STATE_DIR="${NODERAX_STATE_DIR:-/app/.noderax}"
-NODE_MODULES_DIR="/app/node_modules"
-PNPM_DIR="${PNPM_HOME:-/pnpm}"
 FILE_ENV_SUFFIX="_FILE"
 
 apply_file_backed_env() {
@@ -31,11 +29,7 @@ apply_file_backed_env() {
 if [ "$(id -u)" = "0" ]; then
   apply_file_backed_env
   mkdir -p "$STATE_DIR"
-  mkdir -p "$NODE_MODULES_DIR"
-  mkdir -p "$PNPM_DIR"
   chown -R node:node "$STATE_DIR"
-  chown -R node:node "$NODE_MODULES_DIR"
-  chown -R node:node "$PNPM_DIR"
   exec su-exec node "$@"
 fi
 
